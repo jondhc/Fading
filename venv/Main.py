d5c2ea6a6fed1c2e9c179079e6a49776e254f2ab
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2018-10-10 15:34:30 -0500

    finished

--- venv/Main.py
+++ venv/Main.py
@@ -2,7 +2,7 @@ import math
 
 def correction_factor():
     fc = int(input("Input the carrier frequency (Mhz): "))
-    hr = int(input("Input the height of received antenna (Km): "))
+    hr = int(input("Input the height of received antenna (m): "))
     city_size = int(input("Is the city small(1), medium(2) or big(3)? Write the number: "))
     if city_size == 1 or city_size == 2:
         result = (((1.1*math.log10(fc))-0.7) * hr) - ((1.56*log10(fc) - 0.8))
@@ -11,9 +11,22 @@ def correction_factor():
             result = (8.29*(math.pow((math.log10(1.54*hr)), 2))) - 1.1
         elif fc >= 300:
             result = (3.2*(math.pow((math.log10(11.75*hr)), 2))) - 4.97
+    print(result)
     return result
 
-print(correction_factor())
+def path_loss(ahr):
+    area_kind = int(input("Is the area urban(1), suburban(2) or open(3)? Write the number:"))
+    fc = int(input("Input the carrier frequency (Mhz): "))
+    hr = int(input("Input the height of received antenna (m): "))
+    ht = int(input("Input the height of transmitting antenna: "))
+    d = int(input("Input the propagation distance: "))
+    if area_kind == 1:
+        result = 69.55 + (26.16*math.log10(fc)) - (13.82*math.log10(ht)) - ahr + ((44.9-(6.55*math.log10(ht)))*math.log10(d))
+    return result
+
+print(path_loss(correction_factor()))
+
+
 
 
 
